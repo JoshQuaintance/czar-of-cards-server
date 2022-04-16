c91f21178b6c38e6de57438c25957fab7db60f3d
Catalactics <hasianjoshua@gmail.com>
2022-04-16 16:18:00 -0400

    Room Initializationa and host sockets

--- Room.js
+++ Room.js
@@ -1,3 +1,5 @@
+const { Server } = require('socket.io');
+
 class Room {
     #host;
     #roomId;
@@ -6,13 +8,20 @@ class Room {
     #winningScore;
     #playerLimit;
     #inProgress;
+    #port;
+    #gameSocket;
+    #hostSocket;
+
+    #hostPassword;
 
     #playerCount = 0;
     #cardSetInUse = [];
+    #playerList = [];
 
     static rooms = {};
+    static portsInUse = {};
 
-    constructor({ roomId, host, password, idleTimer, winningScore, playerLimit, cardSet = [], inProgress }) {
+    constructor({ roomId, host, password, idleTimer, winningScore, playerLimit, inProgress, hostPassword }) {
         this.#host = host;
         this.#roomId = roomId;
         this.#password = password;
@@ -20,8 +29,29 @@ class Room {
         this.#winningScore = winningScore;
         this.#playerLimit = playerLimit;
         this.#inProgress = inProgress || false;
+        this.#hostPassword = hostPassword;
+
+        this.newPlayer(host);
+        this.#getOpenPort();
+        this.#createSocket();
 
         Room.rooms[roomId] = this;
+    }
+
+    newPlayer(player) {
+        this.#playerCount++;
+        this.#playerList = player;
+    }
+
+    /**
+     * @param {number[]} cardSet
+     */
+    set cardSetInUse(cardSet) {
+        this.#cardSetInUse = cardSet;
+    }
+
+    get port() {
+        return this.#port;
     }
 
     get host() {
@@ -38,6 +68,49 @@ class Room {
 
     get winningScore() {
         return this.#winningScore;
+    }
+
+    #createSocket() {
+        let gameSocket = new Server({
+            cors: {
+                origin: '*',
+            },
+        });
+
+        gameSocket.on('connection', socket => {
+            console.info(`\t New User connected to port ${this.port}`);
+
+            socket.once('identification', data => {
+                let username = data.username;
+                console.log(username);
+                if (username != this.#host) return this.#playerList.push(username);
+
+                if (username == this.#host && data.hostPassword == this.#hostPassword) this.#hostSocket = socket;
+
+                socket.emit('init-player-list', JSON.stringify(this.#playerList));
+            });
+        });
+
+        gameSocket.listen(this.port);
+        console.info(`EVENT: New Room Created with id of ${this.roomId} at port ${this.port}`);
+
+        this.#gameSocket = gameSocket;
+    }
+
+    #getOpenPort() {
+        while (true) {
+            let num = Math.floor(1000 + Math.random() * 9000);
+            if (!Room.portsInUse[num]) {
+                Room.portsInUse[num] = this.roomId;
+                this.#port = num;
+                break;
+            }
+        }
+    }
+
+    #closePort() {
+        delete Room.portsInUse[num];
+        this.#inProgress = false;
     }
 }
 
--- server.js
+++ server.js
@@ -3,25 +3,28 @@ const blacks = require('./cards/cah-black-cards.json');
 const metadata = require('./cards/cah-metadata-cards.json');
 
 const { Server } = require('socket.io');
+const { v4: uuid } = require('uuid');
 const Room = require('./Room');
 
-const io = new Server({
+const newRoomIO = new Server({
     cors: {
         origin: '*',
     },
 });
 
-io.on('connection', socket => {
+newRoomIO.on('connection', socket => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 
     socket.on('create-new-room', data => {
+        let hostPassword = uuid();
+        data.hostPassword = hostPassword;
         let room = new Room(data);
 
-        io.emit('new-room-response', 'Room Created');
+        socket.emit('room-created', { created: true, port: room.port, hostPassword });
     });
 });
 
+newRoomIO.listen(3030);
 
-io.listen(3030);
-console.log('Socket Up! Listening on port 3030');
+console.log('New Room Creation Socket Up! Listening on port 3030');
